21a0f020d5949c0e6821237f6146d68bcdab2bc3
Mariia Honcharenko <m.goncharenkoo@icloud.com>
2024-06-16 08:09:08 -0400

    Update PersonalInfo.js

--- src/PersonalInfo.js
+++ src/PersonalInfo.js
@@ -6,8 +6,7 @@ const PersonalInfo = () => {
       <h2>Personal Information</h2>
       <p>Mariia Honcharenko</p>
       <p>Silver Spring, Maryland</p>
-      <p>646-387-9797</p>
-      <p><a href="mailto:mariiahwork@gmail.com">mariiahwork@gmail.com</a></p>
+      <p>101-232-3234</p>
       <p><a href="https://github.com/mmmaariieee">Github</a></p>
       <p><a href="https://medium.com/@mmmaariieee">Blog</a></p>
     </div>
